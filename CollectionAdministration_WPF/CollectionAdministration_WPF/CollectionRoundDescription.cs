f32f65b4c7c2e7bc59be616791a0f8272c9be0ee
Ginoshie <ginoshie@gmail.com>
2020-03-21 19:33:34 +0100

    Added the collection round (default) descriptions. These descripitions should almost always apply to these collectionrounds. These default descriptions are to be set as the description when the collection round is changed. After the change the user should be free to change the description.

---
+++ CollectionAdministration_WPF/CollectionAdministration_WPF/CollectionRoundDescription.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace CollectionAdministration_WPF
+{
+    public enum CollectionRoundDescription
+    {
+        [Description("Diakonale coll.")]
+        CollectionRound_One,
+        [Description("Instanthoudings coll.")]
+        CollectionRound_Two,
+        [Description("Bestemmings coll.")]
+        CollectionRound_Three
+    }
+}
